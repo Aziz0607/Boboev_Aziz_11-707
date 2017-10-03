bd50de09d979dffc58a52558724b4f0e13f3153c
Азиз Бобоев <aziz.boboev.99@mail.ru>
2017-10-03 16:29:49 +0300

    upload file

---
+++ Example 1.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Автор программы - Бобоев Азизджон 11-707.
+ * 
+ * Что делает программа:
+ *  - Программа меняет местами значения в переменных без использования новой переменной.
+ */
+
+using System;
+
+namespace ConsoleApp
+{
+    class SwapNumbersProgram
+    {
+        public static void Main(string[] args)
+        {
+            int firstNum = 10;
+            int secondNum = 5;
+
+            secondNum = secondNum - firstNum;
+            firstNum = secondNum + firstNum;
+            secondNum = firstNum - secondNum;
+
+            Console.WriteLine("Значение 1-ой переменной: {0}. Значение второй переменной: {1}", firstNum, secondNum);
+            
+            Console.ReadKey();
+        }
+    }
+} 
