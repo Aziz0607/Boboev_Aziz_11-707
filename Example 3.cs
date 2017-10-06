d158e159b8ff356b6b3ecbaaa9b46056a1bc6df3
Азиз Бобоев <aziz.boboev.99@mail.ru>
2017-10-06 06:56:37 +0300

    upload file

---
+++ Example 3.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Автор программы - Бобоев Азизджон. 11-707
+ * 
+ * Описание программы:
+ *  Считает угол между часовой и минутной стрелкой.
+ */
+
+using System;
+
+namespace CalculateAngle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int
+                hours = Convert.ToInt32(Console.ReadLine()),
+                angle = (hours * 30) % 360;
+
+            Console.WriteLine("Угол: {0} градусов", angle);
+            Console.ReadKey();
+        }
+    }
+}
