85dbe5d3800938e847eee3788c1b13bb62141f6d
Азиз Бобоев <aziz.boboev.99@mail.ru>
2017-10-19 14:26:59 +0300

    Upload file

---
+++ Ulearn/Ошибки/ExPr10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 1000 / 3 + 1000 / 5 - 1000 / (3 * 5);
+            Console.WriteLine("Количество чисел! {0}",count);
+        }
+    }
+}
