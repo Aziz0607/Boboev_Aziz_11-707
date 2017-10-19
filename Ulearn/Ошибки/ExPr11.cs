7e163f16743155e857c1dc291ded9245b9eb6b87
Азиз Бобоев <aziz.boboev.99@mail.ru>
2017-10-19 14:38:55 +0300

    Upload file

---
+++ Ulearn/Ошибки/ExPr11.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int
+               hours = Convert.ToInt32(Console.ReadLine()),
+                minutes = Convert.ToInt32(Console.ReadLine()),
+                minuteArrowAngle = (minutes * 6) % 360,
+                hourArrowAngle = (hours * 30) % 360,
+                resultAngle = Math.Abs(hourArrowAngle - minuteArrowAngle);
+
+            Console.WriteLine("Итоговый угол: {0}", resultAngle);
+            Console.ReadKey();
+        }
+    }
+}
