3bf10c8c783f783f87019b7a8fa59b65d5a47e7b
Азиз Бобоев <aziz.boboev.99@mail.ru>
2017-10-22 18:17:48 +0300

    Upload files

---
+++ PR/PR2/Ex,1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class SumOfMultipleNums
+    {
+        public static void Main(string[] args)
+        {
+            int
+                sumMultipleNums = 1000 / 3 + 1000 / 5 - 1000 / (3 * 5);
+
+            Console.WriteLine("Сумма чисел: {0}", sumMultipleNums);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PR/PR3/Ex,3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class IsTicketHappines
+    {
+        public static void Main(string[] args)
+        {
+            int
+                ticketNumber = Convert.ToInt32(Console.ReadLine()),
+                firstTicketPart = ticketNumber / 100000 + ticketNumber / 10000 % 10 + ticketNumber / 1000 % 100 % 10,
+                secondTicketPartDecrement = (ticketNumber - 1) / 100 % 1000 % 100 % 10 + (ticketNumber - 1) / 10 % 10000 % 1000 % 100 % 10 + (ticketNumber - 1) % 100000 % 10000 % 1000 % 100 % 10,
+                secondTicketPartIncrement = (ticketNumber + 1) / 100 % 1000 % 100 % 10 + (ticketNumber + 1) / 10 % 10000 % 1000 % 100 % 10 + (ticketNumber + 1) % 100000 % 10000 % 1000 % 100 % 10;
+
+            Console.WriteLine("Предыдущий билет является: {0}", (firstTicketPart == secondTicketPartDecrement) ? "Счастливым" : "Не счастивым");
+            Console.WriteLine("Следующий билет является: {0}", (firstTicketPart == secondTicketPartIncrement) ? "Счастливым" : "Не счастливым");
+
+            Console.ReadKey();
+        }
+    }
+}
