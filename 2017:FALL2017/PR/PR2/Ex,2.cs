4d800f3fabe26f610a37fa64b410554406b8882c
Азиз Бобоев <aziz.boboev.99@mail.ru>
2017-10-31 15:41:33 +0300

    Upload files

---
+++ 2017:FALL2017/PR/PR2/Ex,2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var days = new string[31];
+            var birthCounts = new double[31];
+
+            // X axis (days) 
+            for (int i = 1; i <= 31; i++)
+            {
+                days[i - 1] = i.ToString();
+            }
+
+            // Y values (counts of birthes) 
+            foreach (var nameSample in names)
+            {
+                if (nameSample.Name == name)
+                {
+                    if (nameSample.BirthDate.Day != 1) birthCounts[nameSample.BirthDate.Day - 1]++;
+                }
+            }
+
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), days, birthCounts);
+        }
+    }
+}
